ebf7642f6e82d6c09edcef288cae12dddf4cfa43
Josh Rivers <josh@joshrivers.me>
2014-06-19 10:21:40 -0700

    Modified bootstrap to use mono 3.4.0 and eventstore 3.0.0rc2.

--- setup/single-bootstrap.sh
+++ setup/single-bootstrap.sh
@@ -1,17 +1,28 @@
 sudo apt-get update
 
-wget http://ha.geteventstore.com/showcase/EventStore-Mono-v2.5.0rc4.tar.gz
+echo Download Eventstore
+wget -nv http://download.geteventstore.com/binaries/eventstore-mono-v3.0.0rc2.tar.gz
+echo Download Mono
+wget -nv http://download.mono-project.com/sources/mono/mono-3.4.0.tar.bz2
 
-tar -xvzf EventStore-Mono-v2.5.0rc4.tar.gz
+sudo apt-get -q -y install g++ make gettext
 
+tar -xvjf mono-3.4.0.tar.bz2
+cd mono-3.4.0/
+cat > mcs/tools/xbuild/targets/Microsoft.Portable.Common.targets <<EOL
+<Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+    <Import Project="..\Microsoft.Portable.Core.props" />
+    <Import Project="..\Microsoft.Portable.Core.targets" />
+</Project>
+EOL
+./configure --prefix=/usr/local; make; make install
+cd ..
 
-if [ ! -f "EventStore-Mono-v2.5.0rc4/singlenode-config.json" ]; then
-  echo "file missing"
-  cat <<EOF > EventStore-Mono-v2.5.0rc4/singlenode-config.json
+tar -xvzf eventstore-mono-v3.0.0rc2.tar.gz
+
+cd EventStore-Mono-v3.0.0rc2/
+cp singlenode-config.dist.json singlenode-config.json
+cat <<EOF > singlenode-config.json
 { ip: "192.168.50.11"}
 EOF
-fi
-
-cd EventStore-Mono-v2.5.0rc4/
-
-./run-singlenode.sh &
+mono-sgen EventStore.SingleNode.exe &
